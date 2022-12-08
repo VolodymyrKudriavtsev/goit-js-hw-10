f7b164e5a7a1b7a8c42709affea136483470dadd
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-09 01:21:38 +0200

    Фільтрація полів

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
-const URL = 'https://restcountries.com/v3.1';
+const URL = 'https://restcountries.com/v3.1/name';
 
 export const fetchCountries = name => {
-  fetch(`${URL}/name/${name}`)
+  fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`)
     .then(response => response.json())
     .then(data => {
       console.log(data);
