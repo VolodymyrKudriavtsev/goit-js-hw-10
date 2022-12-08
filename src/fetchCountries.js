23294b63e05cb47f395f94f99bf51108e507aaaa
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-08 23:51:29 +0200

    http-запит

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,11 @@
+const URL = 'https://restcountries.com/v3.1';
+
 export const fetchCountries = name => {
-  console.log(name);
+  fetch(`${URL}/name/${name}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    });
 };
+
+console.log(fetchCountries('ukr'));
