39e7c264e0e598662c7684a1a2b124a4d97bff73
VolodymyrKudriavtsev <108152577+VolodymyrKudriavtsev@users.noreply.github.com>
2022-12-08 21:51:55 +0000

    Deploying to gh-pages from @ VolodymyrKudriavtsev/goit-js-hw-10@23294b63e05cb47f395f94f99bf51108e507aaaa 🚀

---
+++ index.ed860cb9.js
@@ -0,0 +1,2 @@
+!function(){var n;console.log((n="ukr",void fetch("".concat("https://restcountries.com/v3.1","/name/").concat(n)).then((function(n){return n.json()})).then((function(n){console.log(n)}))))}();
+//# sourceMappingURL=index.ed860cb9.js.map
