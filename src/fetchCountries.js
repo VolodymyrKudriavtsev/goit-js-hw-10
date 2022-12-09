50a077eec3b99299904ed730a96d101cccd763a2
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-12-09 23:47:07 +0200

    render start

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,9 +1,7 @@
 const URL = 'https://restcountries.com/v3.1/name';
 
 export const fetchCountries = name => {
-  fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`)
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-    });
+  return fetch(
+    `${URL}/${name}?fields=name,capital,population,flags,languages`
+  ).then(response => response.json());
 };
--- src/index.js
+++ src/index.js
@@ -3,16 +3,70 @@ import debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
-let country = '';
+let searchBoxValue = '';
+let countries = [];
+
+const getCounrtyListMarkup = ({ flags, name }) => {
+  return `
+  <li class="country-element">
+    <img src="${flags.svg}" alt="Country flag" width="25" />
+    <h class="country-title">${name.common}</h>
+  </li>
+`;
+};
+
+const getCounrtyInfoMarkup = ({
+  flags,
+  name,
+  capital,
+  population,
+  languages,
+}) => {
+  console.log(languages);
+  return `
+  <div class="card-title">
+    <img src="${flags.svg}" alt="Country flag" width="40" />
+    <h>${name.official}</h>
+  </div>
+  <ul class="card-list">
+    <li class="card-element"><span>Capital:</span>${capital}</li>
+    <li class="card-element"><span>Population:</span>${population}</li>
+    <li class="card-element"><span>Languages:</span>${languages}</li>
+  </ul>
+  `;
+};
+
+const renderList = () => {
+  // refs.counrtyList.innerHTML = '';
+  // refs.counrtyInfo.innerHTML = '';
+  const list = countries.map(getCounrtyListMarkup);
+  refs.counrtyList.insertAdjacentHTML('beforeend', list.join(''));
+};
+const renderInfo = () => {};
 
 const refs = {
   searchBox: document.querySelector('#search-box'),
+  counrtyList: document.querySelector('.country-list'),
+  counrtyInfo: document.querySelector('.country-info'),
 };
 
 const onSearchBoxInput = e => {
-  country = e.target.value;
-  console.log(country);
-  fetchCountries(country);
+  searchBoxValue = e.target.value.trim();
+  if (searchBoxValue === '') {
+    refs.counrtyList.innerHTML = '';
+    refs.counrtyInfo.innerHTML = '';
+    return;
+  }
+  fetchCountries(searchBoxValue).then(data => {
+    countries = data;
+    // render();
+    refs.counrtyList.innerHTML = '';
+    refs.counrtyInfo.innerHTML = '';
+    renderList();
+
+    const info = countries.map(getCounrtyInfoMarkup);
+    refs.counrtyInfo.insertAdjacentHTML('beforeend', info.join(''));
+  });
 };
 
 refs.searchBox.addEventListener(
